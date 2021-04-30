f1b291f2aea44caa62a2e0e16d43bde0e6d265aa
Edvard Hultén <edvard.hulten@outlook.com>
2021-04-30 21:06:32 +0200

    Initial commit. Add boilerplate files for new repo

---
+++ README.md
@@ -0,0 +1,14 @@
+# July
+
+
+
+## Why "July"?
+The obvious names `calplot` and `calendarplot` were both already taken by similar packages.
+
+The reasoning was roughly as follows:
+- **Q**: What is this package doing?
+- **A**: Create heatmaps of daily data, grouped by month and year.
+- **Q**: Heat... month... year... Every year is the hottest year ever nowadays, but what is the hottest month?
+- **A**: :sparkle: :sparkle: :sparkle: July :sparkle: :sparkle: :sparkle:  
+
+Also, July is my favourite month. By a light year.
---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="july",
+    version="0.0.1",
+    author="Edvard Hultén",
+    author_email="edvard.hulten@gmail.com",
+    description="Make beautiful heatmaps of daily data.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/e-hulten/july/",
+    keywords=["heatmap", "visualisation", "calendar", "daily", "matplotlib", "github plot"],
+    classifiers=[
+        "Topic :: Scientific/Engineering :: Visualization",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
