c351ce5b0821632e4372486a9f6e61886ef39362
Edvard Hultén <edvard.hulten@outlook.com>
2021-05-01 11:38:23 +0200

    Add month label option

--- src/july/helpers.py
+++ src/july/helpers.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from numpy.typing import ArrayLike
 from typing import List, Tuple, Any, Optional
 from datetime import date
+from datetime import datetime as dt
 
 
 def date_grid(
@@ -41,6 +42,7 @@ def cal_heatmap(
     cmap: str = "Greens",
     colorbar: bool = False,
     date_label: bool = False,
+    month_label: bool = False,
     # grid_lines=True,
     ax: Optional[Tuple[int]] = None,
 ):
@@ -60,6 +62,8 @@ def cal_heatmap(
 
     if date_label:
         add_date_label(ax, dates, flip)
+    if month_label:
+        add_month_label(ax, dates, flip)
     if flip:
         ax.set_yticks([x + 0.5 for x in range(0, 7)])
         ax.set_yticklabels(calendar.weekheader(width=1).split(" "))
@@ -73,7 +77,7 @@ def cal_heatmap(
     return ax
 
 
-def add_date_label(ax, dates: List[date], flip: bool):
+def add_date_label(ax, dates: List[date], flip: bool) -> None:
     days = [day.day for day in dates]
     grid = date_grid(dates, days, flip)
 
@@ -83,3 +87,29 @@ def add_date_label(ax, dates: List[date], flip: bool):
         except ValueError:
             # If date_grid[i, j] is nan.
             pass
+
+
+def add_month_label(ax, dates: List[date], flip: bool) -> None:
+    # Uniquely define months with string tuple (YYYY, MM).
+    month_years = [(day.year, day.month) for day in dates]
+    # Convert tuples to string. Get grid with entries "(YYYY, MM)".
+    month_years_str = list(map(str, month_years))
+    month_year_grid = date_grid(dates, month_years_str, flip, dtype="object")
+
+    unique_month_years = sorted(set(month_years))
+
+    month_locs = {}
+    for month in unique_month_years:
+        # Get 'avg' x, y coordinates of elements in grid equal to month_year.
+        yy, xx = np.nonzero(month_year_grid == str(month))
+        month_locs[month] = (xx.max() + xx.min() if flip else yy.max() + yy.min()) / 2
+
+    # Get month label for each unique month_year.
+    month_labels = [calendar.month_abbr[x[1]] for x in month_locs.keys()]
+
+    if flip:
+        ax.set_xticks([*month_locs.values()])
+        ax.set_xticklabels(month_labels, fontsize=14)
+    else:
+        ax.set_yticks([*month_locs.values()])
+        ax.set_yticklabels(month_labels, fontsize=14, rotation=90)
