cdab235ee40cc5467e77f27c767c0f7b7c543f5f
Edvard Hultén <edvard.hulten@outlook.com>
2021-05-02 22:32:35 +0200

    Add month grid to month plot, new args to calendar heatmap

--- src/july/plot.py
+++ src/july/plot.py
@@ -3,19 +3,23 @@ import calendar
 from typing import List, Any
 from datetime import date
 from july.helpers import date_grid, cal_heatmap, add_date_label, get_month_outline
-from july.utils import preprocess_inputs
+from july.utils import preprocess_inputs, preprocess_month
 
 
 def calendar_heatmap(
     dates,
     data,
     flip=False,
+    title=None,
     cmap="Greens",
     colorbar=False,
     date_label=False,
     weekday_label=True,
     month_label=False,
     year_label=False,
+    month_grid=False,
+    cmin=None,
+    cmax=None,
     ax=None,
 ):
     dates, data = preprocess_inputs(dates, data)
@@ -24,14 +28,19 @@ def calendar_heatmap(
         cal=cal,
         dates=dates,
         flip=flip,
+        title=title,
         cmap=cmap,
         colorbar=colorbar,
         date_label=date_label,
         weekday_label=weekday_label,
         month_label=month_label,
         year_label=year_label,
+        month_grid=month_grid,
+        cmin=cmin,
+        cmax=cmax,
         ax=ax,
     )
+
     return ax
 
 
@@ -39,12 +48,14 @@ def month_plot(
     dates: List[date],
     data: List[Any],
     flip: bool = False,
-    add_day: bool = False,
+    month: int = None,
+    date_label: bool = False,
     cal_mode: bool = False,
     ax=None,
 ):
-    (month,) = set([day.month for day in dates])
-    month_grid = date_grid(dates, data, flip=False)
+    month = month or dates[0].month
+    dates, data = preprocess_month(dates, data, month=month)
+    month_grid = date_grid(dates, data, flip=flip)
     weeknum_grid = date_grid(dates, [d.isocalendar()[1] for d in dates], flip=flip)
     weeknum_labels = [int(x) for x in np.unique(weeknum_grid) if np.isfinite(x)]
 
@@ -63,7 +74,7 @@ def month_plot(
         ax=ax,
     )
 
-    if add_day:
+    if date_label:
         add_date_label(ax, dates, flip)
 
     ax.tick_params(axis="y", pad=8)
@@ -74,9 +85,10 @@ def month_plot(
         ax.set_yticks([i + 0.5 for i in range(month_grid.shape[0])])
         ax.set_yticklabels(weeknum_labels)
 
-    outline_coords = get_month_outline(month_grid)
+    outline_coords = get_month_outline(dates, month_grid, flip, month)
     ax.plot(outline_coords[:, 0], outline_coords[:, 1], color="black", linewidth=2)
-
+    ax.set_xlim(ax.get_xlim()[0] - 0.1, ax.get_xlim()[1] + 0.1)
+    ax.set_ylim(ax.get_ylim()[0] + 0.1, ax.get_ylim()[1] - 0.1)
     ax.set_title(calendar.month_name[month], fontname="monospace", fontsize=14, pad=20)
     ax.set_frame_on(False)
 
