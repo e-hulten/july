87aac91ae47e8870c0f1e680bc77cf8e3ba71f0e
Edvard Hultén <edvard.hulten@outlook.com>
2021-05-06 17:27:17 +0200

    Add calendar plot. Rename cal_heatmap->heatmap

--- src/july/__init__.py
+++ src/july/__init__.py
@@ -2,6 +2,6 @@ __version__ = "0.1.0"
 __author__ = "Edvard Hultén"
 __contact__ = "edvard.hulten@gmail.com"
 
-from july.plot import calendar_heatmap, month_plot
+from july.plot import heatmap, month_plot, calendar_plot
 
-__all__ = ["calendar_heatmap", "month_plot"]
+__all__ = ["heatmap", "month_plot", "calendar_plot"]
